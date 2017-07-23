0a1dc67cd9ebc754366c155a6298fc9cba720a2f
William Ritson <william.ritson@gmail.com>
2017-07-23 11:26:19 -0700

    Added new cards to list

--- cards/allCards.ts
+++ cards/allCards.ts
@@ -1,5 +1,6 @@
 import { Card } from '../card';
 import { makeDamageCard, makeBasicUnit } from './testCards';
+import { makeGrowth1, makeGrowth2 } from './growthCards';
 
 export type CardFactory = () => Card;
 
@@ -7,3 +8,5 @@ export const allCards = new Map<string, CardFactory>();
 
 allCards.set('DamageCard', makeDamageCard);
 allCards.set('BasicUnit', makeBasicUnit);
+allCards.set('G1', makeGrowth1);
+allCards.set('G2', makeGrowth2);
